af1309257d8d5bcbb965075ce201b5dd50ff6146
sammar alaa eldeen <77227853+sammaralaa@users.noreply.github.com>
2022-08-17 21:38:47 +0200

    Add files via upload

---
+++ Day1Task2/Day1Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day1Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2;
+            System.Console.WriteLine("please enter two numbers");
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            int.TryParse(input1, out num1);
+            int.TryParse(input2, out num2);
+            if (num1 > num2)
+            {
+                System.Console.WriteLine(num1);
+            }
+            else
+            {
+                System.Console.WriteLine(num2);
+            }
+        }
+    }
+}
