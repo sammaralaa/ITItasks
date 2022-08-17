14fa984d920b44832aed55f4b777691539f7d252
sammar alaa eldeen <77227853+sammaralaa@users.noreply.github.com>
2022-08-17 21:35:58 +0200

    Add files via upload

---
+++ Day1Task1/Day1Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day1Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("please enter a number");
+            string input1 = System.Console.ReadLine();
+            int.TryParse(input1, out num);
+
+            if (num >= 1 && num <= 10)
+            {
+                System.Console.WriteLine("Valid");
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid");
+            }
+
+        }
+    }
+}
