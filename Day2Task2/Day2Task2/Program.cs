fac58583dfa3746eb499a858b655246d6fb37fa6
sammar alaa eldeen <77227853+sammaralaa@users.noreply.github.com>
2022-08-17 22:24:33 +0200

    Add files via upload

---
+++ Day2Task2/Day2Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day2Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int num, factorial = 1 ;
+            bool isNum;
+            Console.WriteLine("please enter a number");
+            input = Console.ReadLine();
+            isNum = int.TryParse(input, out num);
+            if (isNum)
+            {
+                for(int i = 2; i <= num; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine($"{num}! = {factorial}");
+            }
+        }
+    }
+}
