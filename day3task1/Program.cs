4f8331198a16cc74bc9312e062f0bf68810a4fa2
sammar alaa eldeen <77227853+sammaralaa@users.noreply.github.com>
2022-08-18 23:42:04 +0200

    Add files via upload

---
+++ day3task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+using System.Collections.Generic;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int max, num;
+            Console.WriteLine("please enter a series of numbers separated by a comma.");
+            input = Console.ReadLine();
+           string[] numbers = input.Split(',');
+            max = 0;
+            foreach (string number in numbers) 
+            {
+                if (int.TryParse(number, out num))
+                {
+                    if (num > max)
+                        max = num;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
