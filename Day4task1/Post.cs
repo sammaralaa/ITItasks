87d94e8ac37031e55a2c93036135ddf0dfff4152
sammar alaa eldeen <77227853+sammaralaa@users.noreply.github.com>
2022-08-22 12:23:32 +0200

    Add files via upload

---
+++ Day4task1/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+   public class Post
+    {
+         DateTime creatDate { set; get; }
+        string Title { set; get; }
+        string Description { set; get; }
+        private long VoteValue { set; get; }
+
+      public  Post()
+        {
+            VoteValue = 0;
+        }
+        public void up_vote()
+        {
+            VoteValue += 1;
+        }
+
+        public void down_vote()
+        {
+            if (VoteValue > 0)
+            {
+                VoteValue -= 1;
+            }
+            else
+            {
+                throw (new InvalidOperationException("the vote value is 0 "));
+            }
+        }
+        public long printVotevalue()
+        {
+            return VoteValue;
+        }
+    }
+
---
+++ Day4task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day4task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post p = new Post();
+            p.up_vote();
+            p.up_vote();
+            p.up_vote();
+            p.down_vote();
+            p.down_vote();
+            p.up_vote();
+            p.up_vote();
+            p.up_vote();
+            p.up_vote();
+            Console.WriteLine( p.printVotevalue());
+
+        }
+    }
+}
