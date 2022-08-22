5579737e67a76a8e3ef4a9a1699a745a858d242b
sammar alaa eldeen <77227853+sammaralaa@users.noreply.github.com>
2022-08-22 14:55:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Day5Tsak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string readfile = File.ReadAllText(@"C:\Users\DELL\Documents\file1.txt");
+            string[] words;
+            string longestWord = " ";
+            words = readfile.Split(" ");
+            foreach(string word in words)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+
+            }
+            Console.WriteLine(longestWord);
+        }
+    }
+}
