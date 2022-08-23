1697dec5d6d2cbd9daccc5e42df8cff198c7baef
sammar alaa eldeen <77227853+sammaralaa@users.noreply.github.com>
2022-08-23 14:15:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day4Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack2 ss = new Stack2();
+           // ss.Clear();
+            ss.Push(1);
+            ss.Push(2);
+            ss.Push(3);
+            Console.WriteLine(ss.Pop());
+            Console.WriteLine(ss.Pop());
+            Console.WriteLine(ss.Pop());
+        }
+    }
+}
---
+++ Stack2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    class Stack2: Object
+    {
+    private List<object> objlist;
+
+    public Stack2()
+    {
+        objlist = new List<object>();
+    }
+    public void Push(object obj) { 
+    if(obj!= null)
+        {
+            objlist.Add(obj);
+        }
+        else
+        {
+            throw (new InvalidOperationException("can not store null values"));
+        }
+    }
+    
+    public object Pop()
+    {
+        if (objlist.Count != 0)
+        {
+            int indexOfLast = objlist.Count - 1;
+            object o = objlist[indexOfLast];
+            objlist.RemoveAt(indexOfLast);
+            return o;
+        }
+        else
+        { 
+            throw new Exception("the stack is empty");
+        }
+    }
+
+    public void Clear()
+    {
+        if(objlist.Count != 0)
+        {
+            objlist.Clear();
+        }
+        else
+        {
+            throw (new Exception("the stack is empty"));
+
+        }
+    }
+
+    }
+
